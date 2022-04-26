f68fad65783cc8852e8d137aed0cc7680a94aedb
Šimon Vyhnis <simon.vyhnis@gmail.com>
2022-04-26 21:57:14 +0200

    Added more values to info

--- app/src/main/java/com/simon/orisapp/EventFragment.kt
+++ app/src/main/java/com/simon/orisapp/EventFragment.kt
@@ -10,6 +10,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.core.view.doOnLayout
+import androidx.core.view.doOnNextLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,7 +18,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.simon.orisapp.model.Link
 
 class EventFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val root = inflater.inflate(R.layout.fragment_event, container, false)
         val viewModel = ViewModelProvider(requireActivity()).get(EventListViewModel::class.java)
 
@@ -25,6 +30,11 @@ class EventFragment : Fragment() {
         val date = root.findViewById<TextView>(R.id.date)
         val name = root.findViewById<TextView>(R.id.name)
         val place = root.findViewById<TextView>(R.id.place)
+        val sport = root.findViewById<TextView>(R.id.sport)
+        val discipline = root.findViewById<TextView>(R.id.discipline)
+        val org = root.findViewById<TextView>(R.id.org)
+        val mapName = root.findViewById<TextView>(R.id.map_name)
+        val startTime = root.findViewById<TextView>(R.id.startTime)
 
         val card2 = root.findViewById<CardView>(R.id.cardView2)
         val links = root.findViewById<RecyclerView>(R.id.links)
@@ -41,7 +51,7 @@ class EventFragment : Fragment() {
         name.visibility = View.GONE
 
         viewModel.getEventDetails().observe(viewLifecycleOwner) {
-            if(it.successful) {
+            if (it.successful) {
                 root.findViewById<ProgressBar>(R.id.progress_circular).visibility = View.GONE
                 card1.visibility = View.VISIBLE
                 card2.visibility = View.VISIBLE
@@ -53,18 +63,28 @@ class EventFragment : Fragment() {
                 date.text = "Datum: " + it.data?.date
                 name.text = it.data?.name
                 place.text = "Místo: " + it.data?.place
+                sport.text = "Sport: "+ it.data?.sport?.name
+                discipline.text = "Disciplína: "+ it.data?.discipline?.name
+                org.text = "Organizátor: "+ it.data?.org?.name
+                mapName.text = "Mapa: "+it.data?.map
+                startTime.text = "Start 00: " + it.data?.startTime
 
                 //card2-links
-                it.data?.links?.values.let {noNullLinks->
+                it.data?.links?.values.let { noNullLinks ->
                     links.adapter = LinksViewAdapter(ArrayList<Link?>(noNullLinks))
                     links.layoutManager = LinearLayoutManager(context)
                 }
                 //card3-links
-                it.data?.docs?.values.let {noNullDocs->
+                it.data?.docs?.values.let { noNullDocs ->
                     docs.adapter = LinksViewAdapter(ArrayList<Link?>(noNullDocs))
                     docs.layoutManager = LinearLayoutManager(context)
                 }
-            }else{
+                val map = view?.findViewById<WebView>(R.id.map)
+                map?.doOnLayout {view->
+                    loadMap(root, it.data?.longitude, it.data?.latitude)
+                }
+
+            } else {
                 Toast.makeText(context, R.string.error_msg, Toast.LENGTH_SHORT).show()
             }
         }
@@ -74,23 +94,29 @@ class EventFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //card4-map
+    }
+
+
+    private fun loadMap(view: View, latitude: Float?, longitude: Float?) {
+        print("Lat: $latitude Long: $longitude")
         val map = view.findViewById<WebView>(R.id.map)
-        map.doOnLayout {
-            val html =
-                "<!doctype html><html><head><script src=\"https://api.mapy.cz/loader.js\"></script><script>Loader.load()</script></head>" +
-                        "<body><div id=\"mapa\" style=\"width:${map.width}px; height:${map.height}px;\"></div><script type=\"text/javascript\">" +
-                        "var stred = SMap.Coords.fromWGS84(14.41, 50.08);" +
-                        "var mapa = new SMap(JAK.gel(\"mapa\"), stred, 10);" +
-                        "mapa.addDefaultLayer(SMap.DEF_BASE).enable();" +
-                        "mapa.addDefaultControls();</script></body></html>"
-            val mime = "text/html"
-            val encoding = "utf-8"
-            map.settings.javaScriptEnabled = true
-            map.settings.useWideViewPort = true;
-            map.settings.loadWithOverviewMode = true;
-            map.loadDataWithBaseURL(null, html, mime, encoding, null)
-            print("h: " + map.height + "w: " + map.width)
-        }
+        val html =
+            "<!doctype html><html><head><script src=\"https://api.mapy.cz/loader.js\"></script><script>Loader.load()</script></head>" +
+                    "<body><div id=\"map\" style=\"width:${map.width}px; height:${map.height}px;\"></div><script type=\"text/javascript\">" +
+                    "var stred = SMap.Coords.fromWGS84(${latitude}, ${longitude});" +
+                    "var map = new SMap(JAK.gel(\"map\"), stred, 12);" +
+                    "var layer = new SMap.Layer.Marker();" +
+                    "map.addLayer(layer);" +
+                    "layer.enable();" +
+                    //"var options = {};" + "var marker = new SMap.Marker(stred, \"myMarker\", options);" + "layer.addMarker(marker);"
+                    "map.addDefaultLayer(SMap.DEF_BASE).enable();" +
+                    "map.addDefaultControls();</script></body></html>"
+        val mime = "text/html"
+        val encoding = "utf-8"
+        map.settings.javaScriptEnabled = true
+        map.settings.useWideViewPort = true
+        map.settings.loadWithOverviewMode = true
+        map.loadDataWithBaseURL(null, html, mime, encoding, null)
+        print("h: " + map.height + "w: " + map.width)
     }
 }
--- app/src/main/java/com/simon/orisapp/model/Event.kt
+++ app/src/main/java/com/simon/orisapp/model/Event.kt
@@ -9,6 +9,16 @@ data class Event(@SerializedName("Data") val data : EventData?){
 data class EventData(@SerializedName("Name") val name : String,
                      @SerializedName("Date") val date : String,
                      @SerializedName("Place") val place : String,
+                     @SerializedName("Map") val map : String,
+                     @SerializedName("StartTime") val startTime : String,
+                     @SerializedName("Sport") val sport : Sport,
+                     @SerializedName("Discipline") val discipline : Sport,
+                     @SerializedName("Org1") val org : Org,
                      @SerializedName("Links") val links : Map<String, Link>,
-                     @SerializedName("Documents") val docs : Map<String, Link>
+                     @SerializedName("Documents") val docs : Map<String, Link>,
+                     @SerializedName("GPSLat") val latitude : Float,
+                     @SerializedName("GPSLon") val longitude : Float,
 )
+
+data class Sport(@SerializedName("NameCZ") val name: String)
+data class Org(@SerializedName("Name") val name: String)
